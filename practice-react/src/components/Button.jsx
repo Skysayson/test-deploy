77260014ef4531f563a2f122f96933f4bd41c29f
Sky Sayson <Skysayson74@gmail.com>
2023-10-16 18:24:18 +0800

    #

--- practice-react/src/components/Button.jsx
+++ practice-react/src/components/Button.jsx
@@ -1,79 +1,103 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import './Button.css';
+import "./Button.css";
 
 function Counter() {
-    const [url, setUrl] = useState("");
-    const [count, setCount] = useState(0);
-    const [streak, setStreak] = useState("");
-    const [flag, setFlag] = useState("");
-    const [flagNum, setFlagNum] = useState(0);
-    const streakMsg = "YOU'RE A CUMGOD!";
+  const [url, setUrl] = useState("");
+  const [count, setCount] = useState(0);
+  const [streak, setStreak] = useState("");
+  const [flag, setFlag] = useState("");
+  const [flagNum, setFlagNum] = useState(0);
+  const streakMsg = "YOU'RE A CUMGOD!";
 
-    const fetchData = () => {
-        const apiURL = 'https://api.waifu.im/search';
-        const params = {
-            is_nsfw: true,
-            gif: true
-        };
+  const fetchData = () => {
+    const apiURL = "https://api.waifu.im/search";
+    const params = {
+      is_nsfw: true,
+      gif: true,
+    };
 
-        // Use the URLSearchParams constructor to format the query parameters
-        const queryParameters = new URLSearchParams(params);
-        const requestURL = `${apiURL}?${queryParameters.toString()}`;
+    // Use the URLSearchParams constructor to format the query parameters
+    const queryParameters = new URLSearchParams(params);
+    const requestURL = `${apiURL}?${queryParameters.toString()}`;
 
     axios
-        .get(requestURL)
-        .then((response) => {
-            setUrl(response.data.images[0].url);
-            console.log(response);
-        })
-        .catch((error) => {
-            console.log("Error fetching data");
-        });
+      .get(requestURL)
+      .then((response) => {
+        setUrl(response.data.images[0].url);
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log("Error fetching data");
+      });
+  };
+
+  const showStreak = (count) => {
+    if (count % 10 === 0) {
+      setStreak(streakMsg);
+      setFlagNum(flagNum + 1);
+      setFlag(`x ${flagNum + 1}`);
+    } else {
+      setStreak("");
+      setFlag("");
     }
+  };
 
-    const showStreak = (count) => {
-        if(count % 10 === 0) {
-            setStreak(streakMsg);
-            setFlagNum(flagNum + 1);
-            setFlag(`x ${flagNum + 1}`);
-        } else {
-            setStreak("");
-            setFlag("");
-        }
-    }
+  const myFunction = () => {
+    document.getElementById("myDropdown").classList.toggle("show");
+  };
 
-        console.log(flagNum);
+  return (
+    <div className="Display">
+      <h1 className="word">~NSFW~</h1>
+      <div className="header">
+        <img className="size" src={url}></img>
+      </div>
 
-      return (
-        <div className="Display">
-                <h1 className="word">~NSFW~</h1>
-            <div className="header">
-                <img className="size" src={url}></img>
+      <div className="buttons">
+        <button className="botton" onClick={fetchData}>
+          {" "}
+          Click for a pic!
+        </button>
+
+        <div className="dropdownDiv">
+          <button
+            className="botton3"
+            onClick={() => {
+              myFunction();
+            }}
+          >
+            {" "}
+            Change filters
+          </button>
+
+          <div className="dropdownCont">
+            <div id="myDropdown" className="dropDown">
+              <a href="#">Does</a>
+              <a href="#">Nothing</a>
+              <a href="#">Yet</a>
             </div>
-
-        <div className="buttons">
-            <button className="botton" onClick={fetchData}> Click for a pic!
-            </button>
-
-            <button className="botton3" onClick={fetchData}> Change filters
-            </button>
-        </div>    
-            <div className="fapButton">
-                <button type="button" className="botton2" onClick={() => {
-                    setCount(count + 1); 
-                    showStreak(count + 1);   
-                }}> 
-                    Fap
-                </button>
-                <h4 className="text">Fap Counter {count}</h4>
-            </div>
-            <p className="text2">
-                {streak} {flag}
-            </p>
-                 
+          </div>
         </div>
-      )
-    }
+      </div>
+      <div className="fapButton">
+        <button
+          type="button"
+          className="botton2"
+          onClick={() => {
+            setCount(count + 1);
+            showStreak(count + 1);
+          }}
+        >
+          Fap
+        </button>
+        <h4 className="text">Fap Counter {count}</h4>
+      </div>
+      <p className="text2">
+        {streak} {flag}
+      </p>
+    </div>
+  );
+}
 
 export default Counter;
